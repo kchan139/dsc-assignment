c2f6e6ac440593bb3702969b4d71796521ca9e84
Khoa Tran <79559262+kchan139@users.noreply.github.com>
2023-05-14 10:41:14 +0000

    modified the main function

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ int main ()
 {
     try
     {
-        // string expression;
-        // cout << "Enter an infix expression: ";
-        // getline(cin, expression);
-        // string postfix = Infix2Postfix(expression);
-        // string prefix  = Infix2Prefix(expression);
-        // cout << "Postfix: "    << postfix << endl;
-        // cout << "Prefix: "     << prefix << endl;
+        string expression;
+        cout << "Enter an infix expression: ";
+        getline(cin, expression);
+        string postfix = Infix2Postfix(expression);
+        string prefix  = Infix2Prefix(expression);
+        cout << "Postfix: "    << postfix << endl;
+        cout << "Prefix: "     << prefix << endl;
         cout << "Evaluation: " << PostfixPrefixCalculator("1 +2 +") << endl;
-        // cout << "Evaluation: " << PostfixPrefixCalculator(prefix) << endl;
+        cout << "Evaluation: " << PostfixPrefixCalculator(prefix) << endl;
     }
     catch (const runtime_error & error) 
     {
